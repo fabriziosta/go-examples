96e1cb4a509891b47f45dd6f9c26580aa01438e0
Fabrizio Asta <fasta@redhat.com>
2025-03-02 14:30:07 +0100

    Init commit

---
+++ goroutine/concurrent.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func count() {
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Millisecond * 5)
+	}
+}
+func main() {
+	go count()
+	time.Sleep(time.Millisecond * 20)
+	fmt.Println("Hello World")
+	time.Sleep(time.Millisecond * 10)
+}
